4df80e4ba35f5fb3fa1dce9f6ae9282791bf526e
Sandra <sandramilenasalazar@gmail.com>
2026-04-23 18:17:48 +0200

    Fix: URL dinámica dev/producción para el servidor

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,4 @@
 // Cambia esta IP por la del servidor
-export const API_BASE_URL = 'http://192.168.50.4:5000/api/v1';
+export const API_BASE_URL = __DEV__
+  ? 'http://192.168.50.4:5000/api/v1'
+  : 'https://cinkoingenieria.pythonanywhere.com/api/v1';
